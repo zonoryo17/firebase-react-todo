3e0e134f099262384fa4c6c68bb9961b5d9cfeb9
GomaGoma676 <pga.adam.scott@gmail.com>
2020-09-18 15:54:57 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, List, TextField } from "@material-ui/core";
+import styles from "./App.module.css";
+import { db } from "./firebase";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import TaskItem from "./TaskItem";
+import { makeStyles } from "@material-ui/styles";
 
-function App() {
+import { auth } from "./firebase";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
+
+const App: React.FC = (props: any) => {
+  const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+  const classes = useStyles();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user) => {
+      !user && props.history.push("login");
+    });
+    return () => unSub();
+  });
+
+  useEffect(() => {
+    const unSub = db.collection("tasks").onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
+    return () => unSub();
+  }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input });
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app__root}>
+      <h1>Todo App by React/Firebase</h1>
+      <button
+        className={styles.app__logout}
+        onClick={async () => {
+          try {
+            await auth.signOut();
+            props.history.push("login");
+          } catch (error) {
+            alert(error.message);
+          }
+        }}
+      >
+        <ExitToAppIcon />
+      </button>
+
+      <br />
+      <FormControl>
+        <TextField
+          className={classes.field}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button className={styles.app__icon} disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+
+      <List className={classes.list}>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
+      </List>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TaskItem.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import styles from "./TaskItem.module.css";
+import { ListItem, TextField, Grid } from "@material-ui/core";
+import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import { db } from "./firebase";
+interface PROPS {
+  id: string;
+  title: string;
+}
+
+const TaskItem: React.FC<PROPS> = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const editTask = () => {
+    db.collection("tasks").doc(props.id).set({ title: title }, { merge: true });
+  };
+
+  const deleteTask = () => {
+    db.collection("tasks").doc(props.id).delete();
+  };
+
+  return (
+    <ListItem>
+      <h2>{props.title}</h2>
+      <Grid container justify="flex-end">
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="Edit task"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+      </Grid>
+      <button className={styles.taskitem__icon} onClick={editTask}>
+        <EditOutlinedIcon />
+      </button>
+      <button className={styles.taskitem__icon} onClick={deleteTask}>
+        <DeleteOutlineOutlinedIcon />
+      </button>
+    </ListItem>
+  );
+};
+
+export default TaskItem;
